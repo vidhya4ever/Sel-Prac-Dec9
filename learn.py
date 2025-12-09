7b59a274b2ba29c967c00ec40d2a81ee4d195782
vidhya4ever <vidhya4ever@gmail.com>
2025-12-09 09:59:22 +0530

    created even or odd function

--- learn.py
+++ learn.py
@@ -1,6 +1,8 @@
-n = int(input("Enter the number:    112"))
 
-if n%2==0:
-    print(f"{n} is even")
-else:
-    print(f"{n} is odd")
+def evenodd(n):
+    if n % 2 == 0:
+        print(f"{n} is even")
+    else:
+        print(f"{n} is odd")
+
+evenodd(5)
