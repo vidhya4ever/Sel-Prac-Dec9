74ceb13dde24dd2b3c7c491bf5f38bf88aea5728
vidhya4ever <vidhya4ever@gmail.com>
2025-12-09 10:24:04 +0530

    created class for even of odd prog

--- learn.py
+++ learn.py
@@ -1,9 +1,10 @@
-
-def evenodd(start,end):
+class math_operation:
+ def evenodd(self, start,end):
     for n in range(start,end):
         if n % 2 == 0:
             print(f"{n} is even")
         else:
              print(f"{n} is odd")
 
-evenodd(10,20)
+obj = math_operation()
+obj.evenodd(1,10)
